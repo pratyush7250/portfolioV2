11887e2a6744b53fe6a63e49128ca3612c10cc4d
Pratyush Mohanty <pratyushmohanty7250@gmail.com>
2026-03-20 20:17:22 +0530

    Added new features

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,51 +1,50 @@
-import project1_img from '../assets/food.png'
-import project2_img from '../assets/foodAdmin.png'
-import project3_img from '../assets/udvavisk.png'
-import project4_img from '../assets/safari.png'
-import project5_img from '../assets/chat.png'
-import project6_img from '../assets/project_6.svg'
+import project_food from '../assets/food.png'
+import project_food_admin from '../assets/foodAdmin.png'
+import project_udvavisk from '../assets/udvavisk.png'
+import project_safari from '../assets/safari.png'
+import project_chat from '../assets/chat.png'
+import project_api from '../assets/apibuilder.png'
 
 const mywork_data = [
-    {
+   {
         w_no: 1,
         w_name: "Web design",
-        w_img: project1_img,
-                link: 'https://foodie-wd80.onrender.com/'
+        w_img: project_api,
+        link: 'https://api-builder-85z7.onrender.com/'
 
     },
     {
         w_no: 2,
         w_name: "Web design",
-        w_img: project2_img,
-        link: 'https://foodie-admin-pa99.onrender.com/'
+        w_img: project_udvavisk,
+        link: 'https://www.udvavisk.com/'
 
     },
     {
         w_no: 3,
         w_name: "Web design",
-        w_img: project3_img,
-        link: 'https://www.udvavisk.com/'
+        w_img: project_chat,
+        link: 'https://chitchat-juxw.onrender.com/'
 
     },
-    {
+     {
         w_no: 4,
         w_name: "Web design",
-        w_img: project4_img,
-        link: 'https://safari-hc1d.onrender.com/'
+        w_img: project_food,
+                link: 'https://foodie-wd80.onrender.com/'
 
     },
     {
         w_no: 5,
         w_name: "Web design",
-        w_img: project5_img,
-        link: 'https://chitchat-juxw.onrender.com/'
+        w_img: project_food_admin,
+        link: 'https://foodie-admin-pa99.onrender.com/'
 
-    },
-    {
+    },   {
         w_no: 6,
         w_name: "Web design",
-        w_img: project6_img,
-        link: 'https://yourproject1.com'
+        w_img: project_safari,
+        link: 'https://safari-hc1d.onrender.com/'
 
     },
 ]
