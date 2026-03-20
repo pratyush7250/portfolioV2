5c6dda718949ec6fbef74cf150f804e9e106b241
pratyush7250 <pratyushmohanty7250@gmail.com>
2026-03-20 20:21:27 +0530

    Update mywork_data.js

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -8,41 +8,41 @@ import project_api from '../assets/apibuilder.png'
 const mywork_data = [
    {
         w_no: 1,
-        w_name: "Web design",
+        w_name: "API Builder",
         w_img: project_api,
         link: 'https://api-builder-85z7.onrender.com/'
 
     },
     {
         w_no: 2,
-        w_name: "Web design",
+        w_name: "Udvavisk",
         w_img: project_udvavisk,
         link: 'https://www.udvavisk.com/'
 
     },
     {
         w_no: 3,
-        w_name: "Web design",
+        w_name: "Chat",
         w_img: project_chat,
         link: 'https://chitchat-juxw.onrender.com/'
 
     },
      {
         w_no: 4,
-        w_name: "Web design",
+        w_name: "Food",
         w_img: project_food,
                 link: 'https://foodie-wd80.onrender.com/'
 
     },
     {
         w_no: 5,
-        w_name: "Web design",
+        w_name: "Food Admin",
         w_img: project_food_admin,
         link: 'https://foodie-admin-pa99.onrender.com/'
 
     },   {
         w_no: 6,
-        w_name: "Web design",
+        w_name: "Safari",
         w_img: project_safari,
         link: 'https://safari-hc1d.onrender.com/'
 
