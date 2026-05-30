542d9531f88062ad97dc6df4042684ff7dc5e440
Pratyush Mohanty <pratyushmohanty7250@gmail.com>
2026-05-30 12:33:40 +0530

    new desing added

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,47 +4,63 @@ import project_udvavisk from '../assets/udvavisk.png'
 import project_safari from '../assets/safari.png'
 import project_chat from '../assets/chat.png'
 import project_api from '../assets/apibuilder.png'
+import icon_react from './tech_logo/reactjs.png'
+import icon_angular from './tech_logo/angular.png'
+import icon_nodejs from './tech_logo/nodejs.png'
+import icon_mongo from './tech_logo/mongodb.png'
+import icon_css from './tech_logo/css.png'
+import icon_express from './tech_logo/express.png'
+import icon_tailwind from './tech_logo/tailwindcss.png'
+import icon_wix from './tech_logo/wix.jpg'
+import icon_figma from './tech_logo/figma.png'
 
 const mywork_data = [
-   {
+    {
         w_no: 1,
         w_name: "API Builder",
         w_img: project_api,
-        link: 'https://api-builder-85z7.onrender.com/'
-
+        link: 'https://api-builder-85z7.onrender.com/',
+        tech_icons: [icon_angular, icon_nodejs, icon_mongo, icon_css,icon_figma] // Add icons here
     },
     {
         w_no: 2,
         w_name: "Udvavisk",
         w_img: project_udvavisk,
-        link: 'https://www.udvavisk.com/'
+        link: 'https://www.udvavisk.com/',
+        tech_icons: [icon_wix,icon_figma] // Add icons here
 
     },
     {
         w_no: 3,
         w_name: "Chat",
         w_img: project_chat,
-        link: 'https://chitchat-juxw.onrender.com/'
+        link: 'https://chitchat-juxw.onrender.com/',
+        tech_icons: [icon_react, icon_nodejs, icon_mongo,icon_tailwind,icon_figma] // Add icons here
 
     },
-     {
+    {
         w_no: 4,
         w_name: "Food",
         w_img: project_food,
-                link: 'https://foodie-wd80.onrender.com/'
+        link: 'https://foodie-wd80.onrender.com/',
+        tech_icons: [icon_react, icon_nodejs, icon_mongo, icon_css,icon_figma] // Add icons here
 
     },
     {
         w_no: 5,
         w_name: "Food Admin",
         w_img: project_food_admin,
-        link: 'https://foodie-admin-pa99.onrender.com/'
+        link: 'https://foodie-admin-pa99.onrender.com/',
+        tech_icons: [icon_react, icon_nodejs, icon_mongo, icon_css,icon_figma] // Add icons here
 
-    },   {
+
+    }, {
         w_no: 6,
         w_name: "Safari",
         w_img: project_safari,
-        link: 'https://safari-hc1d.onrender.com/'
+        link: 'https://safari-hc1d.onrender.com/',
+        tech_icons: [icon_react,icon_tailwind,icon_figma] // Add icons here
+
 
     },
 ]
--- src/components/WebApp/Webapp.jsx
+++ src/components/WebApp/Webapp.jsx
@@ -1,40 +1,52 @@
 import React from "react";
 import "./Webapp.css";
-import mywork_data from '../../assets/mywork_data'
-
-
+import mywork_data from '../../assets/mywork_data';
 
 const Webapp = () => {
     return (
-        <section id="web-apps" className="kili-wrapper" >
-
+        <section id="web-apps" className="kili-wrapper">
             <div className="confidence">
                 <h2>Web Apps</h2>
 
                 <div className="mywork-container">
                     {mywork_data.map((work, index) => {
                         return (
-                            <div key={index} className="work-item">
-                                <img src={work.w_img} alt={work.w_name} />
-                                <div className="work-overlay">
-                                    <a
-                                        href={work.link}
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                        className="work-view-btn"
-                                    >
-                                        View Project
-                                    </a>
+                            <div key={index} className="work-card">
+                                {/* Top Image Area with Hover Overlay */}
+                                <div className="work-image-wrapper">
+                                    <img src={work.w_img} alt={work.w_name} className="work-img" />
+                                    <div className="work-overlay">
+                                        <a
+                                            href={work.link}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            className="work-view-btn"
+                                        >
+                                            View
+                                        </a>
+                                    </div>
+                                </div>
+                                
+                                {/* Bottom Tech Icons Footer */}
+                                <div className="work-footer">
+                                    <div className="tech-icons">
+                                        {work.tech_icons && work.tech_icons.map((icon, iconIdx) => (
+                                            <img 
+                                                key={iconIdx} 
+                                                src={icon} 
+                                                alt="tech icon" 
+                                                className="tech-icon-img" 
+                                            />
+                                        ))}
+                                    </div>
                                 </div>
                             </div>
-                        )
+                        );
                     })}
                 </div>
 
                 <button className="confidence-btn">FIND OUT MORE</button>
             </div>
-
-
         </section>
     );
 };
